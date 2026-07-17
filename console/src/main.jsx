c5d251df664d2dca6481abf1b3d514f04b92d8ff
jsanjayvarma06-oss <jsanjay.varma06@gmail.com>
2026-07-17 17:16:01 +0530

    Update main.jsx

--- console/src/main.jsx
+++ console/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Track from './pages/Track';
+import Landing from './pages/Landing';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/track/:orderId" element={<Track />} />
         <Route path="/*" element={<App />} />
       </Routes>
